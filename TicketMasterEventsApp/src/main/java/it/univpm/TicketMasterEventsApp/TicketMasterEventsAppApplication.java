93f0c09e676bfe25f2626afab68ebcef2c48a356
Viorel Saran <viorel.saran@outlook.it>
2022-08-20 16:11:19 +0200

    empty first commit

---
+++ TicketMasterEventsApp/src/main/java/it/univpm/TicketMasterEventsApp/TicketMasterEventsAppApplication.java
@@ -0,0 +1,13 @@
+package it.univpm.TicketMasterEventsApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketMasterEventsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketMasterEventsAppApplication.class, args);
+	}
+
+}
---
+++ TicketMasterEventsApp/src/test/java/it/univpm/TicketMasterEventsApp/TicketMasterEventsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package it.univpm.TicketMasterEventsApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketMasterEventsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
