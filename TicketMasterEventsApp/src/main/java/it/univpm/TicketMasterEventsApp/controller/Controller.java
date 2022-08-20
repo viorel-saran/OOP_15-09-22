2ff33d9196e3b092e1022010d2a5cc2e6b947699
WideHardo7 <marzilianoluca@yahoo.com>
2022-08-20 17:43:38 +0200

    struttura iniziale

--- TicketMasterEventsApp/src/main/java/it/univpm/TicketMasterEventsApp/controller/Controller.java
+++ TicketMasterEventsApp/src/main/java/it/univpm/TicketMasterEventsApp/controller/Controller.java
@@ -1,5 +1,8 @@
 package it.univpm.TicketMasterEventsApp.controller;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
 public class Controller {
 
 }
--- TicketMasterEventsApp/src/main/java/it/univpm/TicketMasterEventsApp/service/Downloadevent.java
+++ TicketMasterEventsApp/src/main/java/it/univpm/TicketMasterEventsApp/service/Downloadevent.java
@@ -1,5 +1,10 @@
 package it.univpm.TicketMasterEventsApp.service;
 
-public class Downloadevent {
+
+
+public class EventDownload {
+	
+	private String apikey="&apikey=U5WWiFt5dP45LjbCYiS7LRp6Fx1P0VSA";
+	
 
 }
