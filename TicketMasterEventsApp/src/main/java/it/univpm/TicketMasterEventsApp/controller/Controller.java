b3317ea656b786df2e1c4cf6de72ecd270a5ec2d
WideHardo7 <marzilianoluca@yahoo.com>
2022-08-22 15:23:28 +0200

    aggiunta dipendenze

--- TicketMasterEventsApp/src/main/java/it/univpm/TicketMasterEventsApp/controller/Controller.java
+++ TicketMasterEventsApp/src/main/java/it/univpm/TicketMasterEventsApp/controller/Controller.java
@@ -1,8 +1,15 @@
 package it.univpm.TicketMasterEventsApp.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class Controller {
 
+	/*
+	@PostMapping(value="Europe")
+	public ResponseEntity<Object> getNumberEurope(@RequestBody String body){
+		
+	}*/
+	
 }
--- TicketMasterEventsApp/src/main/java/it/univpm/TicketMasterEventsApp/service/Downloadevent.java
+++ TicketMasterEventsApp/src/main/java/it/univpm/TicketMasterEventsApp/service/Downloadevent.java
@@ -1,10 +1,18 @@
 package it.univpm.TicketMasterEventsApp.service;
 
+import org.json.simple.JSONArray;
+
 
 
 public class EventDownload {
 	
 	private String apikey="&apikey=U5WWiFt5dP45LjbCYiS7LRp6Fx1P0VSA";
+	
+	public JSONArray Eventieuropei() {
+		
+		
+		
+	}
 	
 
 }
